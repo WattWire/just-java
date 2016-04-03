abfca6cdff1a8293736725117259d07394146da3
Dave Watt <gonebikn@gmail.com>
2016-04-03 15:48:22 -0700

    LapCat push: End of Lesson 2

--- app/src/main/java/net/wattwire/justjava/MainActivity.java
+++ app/src/main/java/net/wattwire/justjava/MainActivity.java
@@ -26,8 +26,11 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Free";
+
+        int price = quantity * 5;
+        String priceMessage = "Amount due: $" + price + ".00 \nThank You!";
         displayMessage(priceMessage);
+
     }
 
     public void increment(View view) {
