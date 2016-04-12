88df5331e9cc85d875d0ead668078ac6e029e861
Dave Watt <gonebikn@gmail.com>
2016-04-11 20:58:41 -0700

    LapCat finished Name Entry.Peets on Monday Evening.

--- app/src/main/java/net/wattwire/justjava/MainActivity.java
+++ app/src/main/java/net/wattwire/justjava/MainActivity.java
@@ -5,44 +5,63 @@ package net.wattwire.justjava;
         import android.util.Log;
         import android.view.View;
         import android.widget.CheckBox;
+        import android.widget.EditText;
         import android.widget.TextView;
 
-/**
- * This app displays an order form to order coffee.
- */
+
+// * This app displays an order form to order coffee.
+
 public class MainActivity extends ActionBarActivity {
 
     boolean wantsWhip = false;
+    boolean nameTap = false;
     int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         displayQuantity(quantity);
 
     }
+//----------------------------------------------------------------------
 
-    /**
-     * This method is called when the order button is clicked.
-     */
+
+//     * This method is called when the order button is clicked.
+
     public void submitOrder(View view) {
 
         int price = quantity * 5;
 
         CheckBox wantsWhipCheckBox = (CheckBox) findViewById(R.id.whipBox);
         boolean wantsWhip = wantsWhipCheckBox.isChecked();
-        String whipState = "Has whipped cream: " + wantsWhip;
+        String whipState = "Whipped Cream: " + wantsWhip;
         Log.v("MainActivity", whipState);
 
         CheckBox wantsChocoCheckBox = (CheckBox) findViewById(R.id.chocoBox);
         boolean wantsChoco = wantsChocoCheckBox.isChecked();
-        String chocoState = "Wants Chocolate? " + wantsChoco;
+        String chocoState = "Chocolate: " + wantsChoco;
         Log.v("MainActivity", chocoState);
 
-        String priceMessage = whipState + "\n" + chocoState + "\n" + "Amount: $" + price + ".00 \nThank You!";
-        displayMessage(priceMessage);
+        String priceMessage = whipState + "\n";
+        priceMessage += chocoState + "\n";
+        priceMessage += "Quantity: " + quantity + "\n";
+        priceMessage += "Total: $" + price + ".00 \n";
+        priceMessage +=  "Thank You!";
 
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(priceMessage);
+
+    }
+
+    public void clearName(View view) {
+
+        if (!nameTap) {
+            EditText nameText = (EditText) findViewById(R.id.editName);
+            nameText.setText("");
+            nameTap = true;
+        }
     }
 
     public void increment(View view) {
@@ -59,21 +78,21 @@ public class MainActivity extends ActionBarActivity {
         displayQuantity(quantity);
     }
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
+ //   * This method displays the given quantity value on the screen.
+
     private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+
+//      If quantity is updated, go into Pre-Order State: Wipe Out Prev Order Info.
+
+        int price = quantity * 5;
+
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText("$" + price + ".00");
+
     }
 
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
 
 }
