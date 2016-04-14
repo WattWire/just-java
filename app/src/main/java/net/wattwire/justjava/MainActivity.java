91a4b05f1e0ae03c21ee3b4e0b6bc46e5fae2553
Dave Watt <gonebikn@gmail.com>
2016-04-13 20:58:45 -0700

    LapCat-->Got Email Intent Working (Peet's, Weds Eve.)!!!!!!!!!!

--- app/src/main/java/net/wattwire/justjava/MainActivity.java
+++ app/src/main/java/net/wattwire/justjava/MainActivity.java
@@ -1,6 +1,7 @@
 package net.wattwire.justjava;
 
         import android.content.Intent;
+        import android.net.Uri;
         import android.os.Bundle;
         import android.provider.AlarmClock;
         import android.support.v7.app.ActionBarActivity;
@@ -28,14 +29,10 @@ public class MainActivity extends ActionBarActivity {
         displayQuantity();
 
     }
-
-
 //----------------------------------------------------------------------
-//     * This method is called when the ORDER button is clicked.
-    public void submitOrder(View view) {
+//----------------------------------------------------------------------
 
-        String message = "Your Time is Up!!!";
-        int seconds = 4;
+    public void startTimer(String message, int seconds) {
 
         Intent intent = new Intent(AlarmClock.ACTION_SET_TIMER)
                 .putExtra(AlarmClock.EXTRA_MESSAGE, message)
@@ -44,28 +41,62 @@ public class MainActivity extends ActionBarActivity {
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
+    }
 
+    public void showMap(Uri geoLocation) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 
-//        CheckBox wantsWhipCheckBox = (CheckBox) findViewById(R.id.whipBox);
-//        boolean wantsWhip = wantsWhipCheckBox.isChecked();
-//        String whipState = "Whipped Cream: " + wantsWhip;
-//
-//        CheckBox wantsChocoCheckBox = (CheckBox) findViewById(R.id.chocoBox);
-//        boolean wantsChoco = wantsChocoCheckBox.isChecked();
-//        String chocoState = "Chocolate: " + wantsChoco;
-//
-//        EditText editName = (EditText) findViewById(R.id.edit_text_name);
-//
-//        String priceMessage = "Name: " + editName.getText() + "\n";
-//        priceMessage += whipState + "\n";
-//        priceMessage += chocoState + "\n";
-//        priceMessage += "Quantity: " + quantity + "\n";
-//        priceMessage += "Total: $" + calcTotal() + ".00 \n";
-//        priceMessage +=  "Thank You!";
-//
-//        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-//        orderSummaryTextView.setText(priceMessage);
+    public void composeEmail(String[] addresses, String subject, String body) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, body);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 
+//----------------------------------------------------------------------
+//     * This method is called when the ORDER button is clicked.
+    public void submitOrder(View view) {
+
+//          startTimer("You time iz Zup!", 3);
+
+//        Uri mapLoc = Uri.parse("geo:0,0?q=220+Union+Ave%2C+CA+95032");
+//        showMap(mapLoc);
+
+        CheckBox wantsWhipCheckBox = (CheckBox) findViewById(R.id.whipBox);
+        boolean wantsWhip = wantsWhipCheckBox.isChecked();
+        String whipState = "Whipped Cream: " + wantsWhip;
+
+        CheckBox wantsChocoCheckBox = (CheckBox) findViewById(R.id.chocoBox);
+        boolean wantsChoco = wantsChocoCheckBox.isChecked();
+        String chocoState = "Chocolate: " + wantsChoco;
+
+        EditText editName = (EditText) findViewById(R.id.edit_text_name);
+
+        String priceMessage = "Name: " + editName.getText() + "\n";
+        priceMessage += whipState + "\n";
+        priceMessage += chocoState + "\n";
+        priceMessage += "Quantity: " + quantity + "\n";
+        priceMessage += "Total: $" + calcTotal() + ".00 \n";
+        priceMessage +=  "Thank You!";
+
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(priceMessage);
+
+//***** For Now, We'll Display the order message for debugging + Emailing the Order:
+        String[] destAddress = new String[1];
+        destAddress[0] = "gonebikn@gmail.com";
+        String orderSubject = "Just Java Order";
+
+        composeEmail(destAddress, orderSubject, priceMessage);
 
     }
 
